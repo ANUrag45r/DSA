6fd43d038370da06b45bebdfc610abcc7838576f
Anurag sinha <anuragsinha067@gmail.com>
2026-07-30 16:09:14 +0530

    30/07/2026

---
+++ GeeksForGeeks/dynamic programming/MIN_insert_delete.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    string solve(string &s1, string &s2, int i, int j, vector<vector<string>>& dp) {
+
+        if (i < 0 || j < 0) {
+            return "";
+        }
+
+        if (dp[i][j] != "#") {
+            return dp[i][j];
+        }
+
+        if (s1[i] == s2[j]) {
+            return dp[i][j] = string(1, s1[i]) + solve(s1, s2, i - 1, j - 1, dp);
+        }
+
+        string left = solve(s1, s2, i - 1, j, dp);
+        string right = solve(s1, s2, i, j - 1, dp);
+
+        if (left.size() > right.size()) {
+            return dp[i][j] = left;
+        }
+
+        return dp[i][j] = right;
+    }
+
+public:
+    int minOperations(string &s1, string &s2) {
+
+        int n = s1.size();
+        int m = s2.size();
+
+        vector<vector<string>> dp(n, vector<string>(m, "#"));
+
+        string lcs = solve(s1, s2, n - 1, m - 1, dp);
+
+        int ans = (n - lcs.size()) + (m - lcs.size());
+
+        return ans;
+    }
+};
