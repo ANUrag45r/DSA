44e925319cfac963f6595055e71b7eb9bb13c490
Anurag sinha <anuragsinha067@gmail.com>
2026-07-31 20:13:21 +0530

    31/07/2026

---
+++ interview_questions/1.cpp
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> st;
+
+        int i = 0, j = 0;
+        int n = s.size();
+        int ans = 0;
+
+        while (j < n) {
+
+            if (st.find(s[j]) == st.end()) {
+                st.insert(s[j]);
+                ans = max(ans, j - i + 1);
+                j++;
+            } else {
+                st.erase(s[i]);
+                i++;
+            }
+        }
+
+        return ans;
+    }
+};
---
+++ interview_questions/2.cpp
@@ -0,0 +1,45 @@
+//https://leetcode.com/problems/longest-palindromic-substring/
+
+
+class Solution {
+private:
+    bool isPalindrome(string &s, int i, int j, vector<vector<int>> &dp) {
+
+        if (i >= j)
+            return true;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (s[i] != s[j])
+            return dp[i][j] = false;
+
+        return dp[i][j] = isPalindrome(s, i + 1, j - 1, dp);
+    }
+
+public:
+    string longestPalindrome(string s) {
+
+        int n = s.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        int start = 0;
+        int maxLen = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+
+                if (isPalindrome(s, i, j, dp)) {
+
+                    if (j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
+                        start = i;
+                    }
+                }
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
